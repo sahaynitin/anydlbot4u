c1c6a86ea975c7c24a067286c9c0bbd6da546a3e
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-07-10 11:17:17 +0530

    Update FFMpegRoBot.py

--- plugins/FFMpegRoBot.py
+++ plugins/FFMpegRoBot.py
@@ -50,7 +50,7 @@ async def ffmpegrobot_ad(bot, update):
     )
 
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["trim1"]))
+@pyrogram.Client.on_message(pyrogram.Filters.command(["tri"]))
 async def trim(bot, update):
     if update.from_user.id not in Config.AUTH_USERS:
         await bot.delete_messages(
