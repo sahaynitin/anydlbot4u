215727cc262ff9a5772b384f25ab861e602afad8
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-07-12 15:29:04 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -132,7 +132,7 @@ async def rename_doc(bot, update):
             await bot.edit_message_text(
                 text=Translation.AFTER_SUCCESSFUL_UPLOAD_MSG,
                 chat_id=update.chat.id,
-                message_id=ab.message_id,
+                message_id=update.message_id,
                 disable_web_page_preview=True
             )
     else:
