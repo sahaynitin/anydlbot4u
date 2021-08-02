05b6ae174b44250236aae4735d1d06fdb9689a00
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-08-02 20:40:18 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -36,7 +36,7 @@ from PIL import Image
 from helper_funcs.help_Nekmo_ffmpeg import generate_screen_shots
 
 from pyrogram import Client, filters
-from pyrogram.types import CallbackQuery
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
 async def youtube_dl_call_back(bot, update):
     cb_data = update.data
