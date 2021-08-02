de6cdabe9b7fb52ddf58d9ae4d871175f774706c
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-08-02 14:04:19 +0530

    Create callback_handler.py

---
+++ plugins/callback_handler.py
@@ -0,0 +1,20 @@
+import asyncio
+import os
+from pyrogram import Client, filters
+from pyrogram.types import CallbackQuery3
+from youtube_dl_button import download_directory
+from helper_funcs.display_progress import humanbytes
+
+if bool(os.environ.get("ENV", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+@Client.on_callback_query(filters.regex(r'^progress$'))
+async def ytdl_progress(bot, cb: CallbackQuery):
+    file_siz = humanbytes(os.getsize(download_directory))
+    try:
+        os.getsize(download_directory))
+    except Exception:
+        pass
+    await cb.answer(f"Downloaded file size : {file_siz}", True)
