454b8f4e18d750db45814ce04f1234f02bb1c125
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-08-02 20:30:22 +0530

    Update callback_handler.py

--- plugins/callback_handler.py
+++ plugins/callback_handler.py
@@ -1,3 +1,4 @@
+'''
 import asyncio
 import os
 from pyrogram import Client, filters
@@ -8,7 +9,7 @@ if bool(os.environ.get("ENV", False)):
 else:
     from config import Config
 
-'''
+
 @Client.on_callback_query(filters.regex(r'^progress$'))
 async def ytdl_progress(bot, cb: CallbackQuery):
     file_siz = pgress()
