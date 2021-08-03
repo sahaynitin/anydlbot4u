36d43bafcee0c02d06f70b326a601262e23e6802
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-08-03 19:16:21 +0530

    Update youtube_dl_button.py

--- plugins/youtube_dl_button.py
+++ plugins/youtube_dl_button.py
@@ -39,18 +39,6 @@ from helper_funcs.help_Nekmo_ffmpeg import generate_screen_shots
 from pyrogram import Client, filters
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
 
-@pyrogram.Client.on_callback_query(filters.regex(r'^progress$'))
-async def ytdl_progress(bot, cb: CallbackQuery):
-    file_siz = humanbytes(os.path.getsize(download_directory))
-    try:
-        os.path.getsize(download_directory)
-    except Exception:
-        await cb.answer("File Not Found", True)
-        print('Error aa gaya')
-        pass
-    #file_siz = pgress()
-    await cb.answer(f"Downloaded file size : {file_siz}", True)
-
 
 async def youtube_dl_call_back(bot, update):
     cb_data = update.data
@@ -378,3 +366,15 @@ async def youtube_dl_call_back(bot, update):
                 disable_web_page_preview=True
             )
     
+
+@pyrogram.Client.on_callback_query(filters.regex(r'^progress$'))
+async def ytdl_progress(bot, cb: CallbackQuery):
+    file_siz = humanbytes(os.path.getsize(download_directory))
+    try:
+        os.path.getsize(download_directory)
+    except Exception:
+        await cb.answer("File Not Found", True)
+        print('Error aa gaya')
+        pass
+    #file_siz = pgress()
+    await cb.answer(f"Downloaded file size : {file_siz}", True)
