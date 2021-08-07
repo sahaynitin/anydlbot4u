cb88cbceff0615281bf9ae48a6fd12beef0c7147
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-08-07 10:16:56 +0530

    Update callback_handler.py

--- plugins/callback_handler.py
+++ plugins/callback_handler.py
@@ -3,10 +3,11 @@ import os
 from pyrogram import Client, filters
 from pyrogram.types import CallbackQuery
 
-
+'''
 ads="PROGRESS..."
 @Client.on_callback_query(filters.regex(r'^progress$'))
 async def ytdl_progress(bot, cb: CallbackQuery):
     print('Pgreasc'*10)
     await cb.answer(f"{ads}", True)
 
+'''
