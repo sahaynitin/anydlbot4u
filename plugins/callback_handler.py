c1b199c4975965a087f5a4cfa301fae7739d6a9b
Mynameisuaername <77132870+Mynameisuaername@users.noreply.github.com>
2021-08-19 10:29:15 +0530

    Update callback_handler.py

--- plugins/callback_handler.py
+++ plugins/callback_handler.py
@@ -4,11 +4,12 @@ from pyrogram import Client, filters
 from pyrogram.types import CallbackQuery
 from plugins.youtube_dl_button import d_directory
 
-
+'''
 ads = d_directory()
 @Client.on_callback_query(filters.regex(r'^progress$'))
 async def ytdl_progress(bot, cb: CallbackQuery):
     print('Pgreasc'*10)
     await cb.answer(f"Downloaded : {ads}", True)
-
+'''   
+ 
 
