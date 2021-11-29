4ef8f1c3bca690f5ceb47f7546a69b5d51ace16b
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-29 19:42:09 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -36,3 +36,31 @@ class Config(object):
     PROCESS_MAX_TIMEOUT = 3600
     # watermark file
     DEF_WATER_MARK_FILE = ""
+    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
+    # the download location, where the HTTP Server runs
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+    # Telegram maximum file upload size
+    MAX_FILE_SIZE = 50000000
+    TG_MAX_FILE_SIZE = 2097152000
+    FREE_USER_MAX_FILE_SIZE = 50000000
+    # chunk size that should be used with requests
+    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
+    # default thumbnail to be used in the videos
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "")
+    # proxy for accessing youtube-dl in GeoRestricted Areas
+    # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
+    HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
+    # maximum message length in Telegram
+    MAX_MESSAGE_LENGTH = 4096
+    # set timeout for subprocess
+    PROCESS_MAX_TIMEOUT = 3600
+    # watermark file
+    DEF_WATER_MARK_FILE = ""
+    # your telegram id
+    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
+    # Update channel for Force Subscribe
+    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
+    # database session name, example: xurluploader
+    SESSION_NAME = os.environ.get("SESSION_NAME", "")
+    # database uri (mongodb)
+    DATABASE_URL = os.environ.get("DATABASE_URL", "")
